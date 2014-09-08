4c644feefece2e500dc285a4dae208a2a75c0c6f
SavioLopes <saviooliveiralopess@yahoo.com.br>
2014-09-08 00:50:02 -0300

    teste

--- server.js
+++ server.js
@@ -2,8 +2,6 @@
 
 var http = require('http');
 http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello Node.JS!');
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello Node.JS!');
 }).listen(process.env.OPENSHIFT_NODEJS_PORT);
-
-
